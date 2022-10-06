33c9219c51c539e4446fc14b3a3e44606c5da8e9
meagan <student6@codeandcircuit.org>
2022-10-06 17:37:18 -0400

    MORE STUFF

--- custom.js
+++ custom.js
@@ -1,3 +1,11 @@
 alert("WORNING: DIS WEBSIT IS VIRY ILEGAL DON TELL ANYBODY OR I WIL COM AND GIT U")
 alert("U MIST FELLO AL INSTRUCTINS")
 alert("and dont forget marc is a salmon")
+$(document).ready(function() {
+    $('h1').css('color','white')
+    for (let i = 0; i < 10000; i++) {
+        $('#robux').click(()=>{
+            alert("KLIK WTHE BUTTIN")
+            })
+    }
+})
